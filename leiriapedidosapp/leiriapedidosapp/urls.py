40bee718c779c87b2f0911ccd52b6a752524c503
vicEduDev <victoreduardo2203@gmail.com>
2025-05-07 23:05:00 -0300

    Dando os primeiros passos com o front

--- leiriapedidosapp/leiriapedidosapp/urls.py
+++ leiriapedidosapp/leiriapedidosapp/urls.py
@@ -1,9 +1,7 @@
-# filepath: c:\Users\victo\Desktop\leiriapedidosapp\leiriapedidosapp\leiriapedidosapp\urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('homepage.urls')),
+    path('', include('homepage.urls')),
 ]
