829999b8f50fc4ad869d88901113f58bed1454dc
vicEduDev <victoreduardo2203@gmail.com>
2025-05-08 19:35:15 -0300

    Mudanças no navbar.html além de começar métodos de login e logout

--- leiriapedidosapp/homepage/views.py
+++ leiriapedidosapp/homepage/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
