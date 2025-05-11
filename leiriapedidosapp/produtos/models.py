610c2ce4400fb176cde767b9d95254ad9fb57aad
vicEduDev <victoreduardo2203@gmail.com>
2025-05-11 18:39:49 -0300

    Criado novamente arquivo mydb.py para ser executado uma vez

--- leiriapedidosapp/produtos/models.py
+++ leiriapedidosapp/produtos/models.py
@@ -1,28 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Produto(models.Model):
-    CATEGORIA_CHOICES = [
-        ('BOLO', 'Bolo'),
-        ('DOCE', 'Doce'),
-        ('SALGADO', 'Salgado'),
-        ('TORTA', 'Torta'),
-        ('MASSA', 'Massa'),
-    ]
-    nome = models.CharField(max_length=100)
-    categoria = models.CharField(max_length=20, choices=CATEGORIA_CHOICES)
-    preco_unitario = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        blank=True,
-        null=True,
-        verbose_name='Preço Unitário')
-    preco_kg = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        blank=True,
-        null=True,
-        verbose_name='Preço por Kg')
-
+    nome = models.CharField(max_length=50)
+    observacoes = models.CharField(max_length=150)
+    
     def __str__(self):
         return self.nome
+    
+class PrdutoSalgado(Produto):
