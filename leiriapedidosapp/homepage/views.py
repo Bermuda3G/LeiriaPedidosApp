f5e521c54b455215013155485f65a744407f27b7
vicEduDev <victoreduardo2203@gmail.com>
2025-05-11 02:41:43 -0300

    Adicionada lógica de login e mensagens de login

--- leiriapedidosapp/homepage/views.py
+++ leiriapedidosapp/homepage/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'homepage.html', {})
+    #Checa se o usuário está loggado
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Autenticar
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Você foi loggado com sucesso!!")
+            return redirect('homepage')
+        else:
+            messages.success(request, "Não foi possível concluir o processo de Login. Tente novamente.")
+            return redirect('homepage')
+    else:
+        return render(request, 'homepage.html', {})
 
 #def login_user(request):
     #pass
