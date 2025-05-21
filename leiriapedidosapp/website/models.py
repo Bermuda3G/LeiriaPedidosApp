3558f5aa9e659f1cb5219b671c4bfba161f21a47
vicEduDev <victoreduardo2203@gmail.com>
2025-05-21 19:51:27 -0300

    Criada model de Pedidos e model  manytomany intermediária ItemPedido

--- leiriapedidosapp/website/models.py
+++ leiriapedidosapp/website/models.py
@@ -19,3 +19,26 @@ class Produto(models.Model):
 
     def __str__(self):
         return(f"{self.nome}")
+    
+class Pedido(models.Model):
+    nome_cliente = models.CharField(max_legnth=50)
+    numero_cliente = models.CharField(max_length=20)
+    endereco_cleinte = models.TextField(max_length=100)
+    endereco_entrega = models.TextField(max_length=100)
+    produtos = models.ManyToManyField(Produto, through="ItemPedido")
+    valor = models.DecimalField(default=0.00, max_digits=6, decimal_places=2)
+    observacoes = models.TextField(max_length=100)
+
+    def __str__(self):
+        return(f"Pedido #{self.id}")
+    
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return(f"{self.produto.nome} X {self.quantidade}")
